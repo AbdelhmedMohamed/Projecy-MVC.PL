678a5796375b9eb1f3ed8ee897288a110245c53d
DELL <DELL@DESKTOP-AVJE8K2>
2024-09-10 04:50:20 +0300

    Ass-C42-G02-MVC05

---
+++ ProjectMVC.DAL/Models/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMVC.DAL.Models
+{
+   public enum Gender
+    {
+        [EnumMember(Value ="Male")]
+        Male=1 ,
+        [EnumMember(Value = "Female")]
+
+        Female = 2
+    }
+
+    enum EmployeeType
+    {
+        FullTime =1 ,   
+        PartTime=2  
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required")]
+        [MaxLength(50,ErrorMessage = "Max Length For Namr Is 50")]
+        [MinLength(4,ErrorMessage = "Max Length For Namr Is 4")]
+
+        public string Name { get; set; }
+
+        [Range(21,60)]
+
+        public int? Age {  get; set; }
+        [RegularExpression(@"^[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$"
+         , ErrorMessage ="Address Must Be Like 123-street-city-country" )]
+
+        public string Address { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salary {  get; set; }
+
+        public bool IsActive { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name ="phone Number")]
+
+        public string PhoneNumber { get; set; }
+
+        [Display(Name ="Hire Date")]
+
+        public DataType HireDate { get; set; }
+
+        public bool IsDeleted {  get; set; }    
+
+        public Gender Gender { get; set; }
+
+
+    }
+}
--- Projecy MVC.PL/Controllers/DepartmentController.cs
+++ Projecy MVC.PL/Controllers/DepartmentController.cs
@@ -1,21 +1,147 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using ProjectMVC.BLL.Interfacies;
+using ProjectMVC.DAL.Models;
+using System;
 
 namespace Projecy_MVC.PL.Controllers
 {
     public class DepartmentController : Controller
     {
         private readonly IDepartementRepository _departmentRepository;
+        private readonly IWebHostEnvironment _env;
 
-        public DepartmentController(IDepartementRepository repository)
+        public DepartmentController(IDepartementRepository repository, IWebHostEnvironment env)
         {
             _departmentRepository = repository;
+            _env = env;
         }
+
         public IActionResult Index()
         {
-            _departmentRepository.GetAll();
+            var departments = _departmentRepository.GetAll();
 
+            return View(departments);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var count = _departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id, string ViewName = "Details")
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+            var department = _departmentRepository.GetById(id.Value);
+            if (department == null) {
+                return NotFound();
+            }
+            return View(ViewName, department);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (!id.HasValue)
+            //{
+            //    return BadRequest();
+            //}
+            //var department = _departmentRepository.GetById(id.Value);
+            //if (department == null)
+            //{
+            //    return NotFound();
+            //}
+            //return View(department); 
+
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int id, Department department)
+        {
+            if (id != department.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid) {
+                return View(department);
+            }
+
+            try
+            {
+                _departmentRepository.Update(department);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                if (_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                else {
+                    ModelState.AddModelError(string.Empty, "An Erorr occured during update depatrment");
+                }
+                return View(department);
+            }
+        }
+
+        public IActionResult Delete(int? id) {
+
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Department department)
+        {
+            try
+            {
+                _departmentRepository.Delete(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                if (_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "An Erorr occured during deleting depatrment");
+                }
+                return View(department);
+            }
+        }
+
+
+
+
     }
 }
